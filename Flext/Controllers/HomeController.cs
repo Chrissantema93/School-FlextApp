35c13bc43eeced83d8d991ebf42716821a6cce57
Sebastiaan van Etten <vanettensebastiaan@gmail.com>
2018-06-12 15:25:49 +0200

    comments in de imagecontroller leggen het beter uit
    hoi

--- Flext/Controllers/HomeController.cs
+++ Flext/Controllers/HomeController.cs
@@ -10,6 +10,15 @@ namespace Flext.Controllers
 {
     public class HomeController : Controller
     {
+
+
+        IDescriptionRepository DesciptionRepo;
+        public HomeController(IDescriptionRepository idr)
+        {
+            DesciptionRepo = idr;
+        }
+
+
         public IActionResult Index()
         {
             return View();
@@ -18,11 +27,27 @@ namespace Flext.Controllers
         [HttpGet]
         public IActionResult Overzicht()
         {
+            //hier worden alle detecties uit de databaase gehaald die niet ouder zijn dan 5 minuten
+            var query = DesciptionRepo.Detecties.Where(x => x.Timestamp > DateTime.Now.AddMinutes(-5));
+
 
             TafelStatus Status = new TafelStatus { Tafelnaam = "TestTafel", Stoelen = new List<StoelInfo>() };
             for (int i = 0; i < 8; i++)
             {
-                Status.Stoelen.Add(new StoelInfo { Bezet = TorF() });
+                // wat die hier doet is hij kijkt in de query die hierboven wordt uitgevoerd of er een record is voor de specifieke stoel, 
+                // hij kijkt NIET naar de inhoud van record (de tags).
+                // zolang de record er is zet hij hem op bezet.
+                // hier moet dus de SVM tussen komen doormiddel van een api call ofzo. 
+                // er moet dus nog in worden gebouwd dat er naar de inhoud word gekeken en aan de hand daarvan op bezet worden gezet.
+                if (query.Where(x => x.StoelId == i+1).FirstOrDefault() != null)
+                {
+                    Status.Stoelen.Add(new StoelInfo { Bezet = true });
+                }
+                else
+                {
+                    Status.Stoelen.Add(new StoelInfo { Bezet = false });
+                }
+                
             }
             return View(Status);
         }
